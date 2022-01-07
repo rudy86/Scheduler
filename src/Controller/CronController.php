80f0eef986b3dcc06a8c5eb23c95f28b37c10e80
rlensen <r.lensen@connectingthedots.nl>
2022-01-07 13:44:48 +0100

    composer.json

--- src/Controller/CronController.php
+++ src/Controller/CronController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Rudy86\SchedulerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
